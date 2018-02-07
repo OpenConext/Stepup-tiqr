772b974493b646895ee75cc938305fb6a9f06ca8
Eric Pinxteren <epinxteren@ibuildings.nl>
2018-02-07 12:15:25 +0100

    Create it's own registration controller

--- src/AppBundle/Controller/RegistrationController.php
+++ src/AppBundle/Controller/RegistrationController.php
@@ -24,7 +24,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
-class DefaultController extends Controller
+class RegistrationController extends Controller
 {
     private $authenticationService;
     private $registrationService;
@@ -38,43 +38,30 @@ class DefaultController extends Controller
     }
 
     /**
-     * Replace this example code with whatever you need/
-     *
-     * @Route("/", name="homepage")
-     */
-    public function indexAction()
-    {
-        return $this->render('default/index.html.twig');
-    }
-
-    /**
      * Replace this example code with whatever you need.
      *
-     * See @see AuthenticationService for a more clean example.
+     * See @see RegistrationService for a more clean example.
      *
-     * @Route("/authentication", name="app_identity_authentication")
+     * @Route("/registration", name="app_identity_registration")
      */
-    public function authenticationAction(Request $request)
+    public function registrationAction(Request $request)
     {
-        $nameId = $this->authenticationService->getNameId();
-
         if ($request->get('action') === 'error') {
-            $this->authenticationService->reject($request->get('message'));
-            return $this->authenticationService->replyToServiceProvider();
+            $this->registrationService->reject($request->get('message'));
+            return $this->registrationService->replyToServiceProvider();
         }
 
-        if ($request->get('action') === 'authenticate') {
-            // The application should very if the user matches the nameId.
-            $this->authenticationService->authenticate();
-            return $this->authenticationService->replyToServiceProvider();
+        if ($request->get('action') === 'register') {
+            $this->registrationService->register($request->get('NameID'));
+            return $this->registrationService->replyToServiceProvider();
         }
 
-        $requiresAuthentication = $this->authenticationService->authenticationRequired();
-        $response = new Response(null, $requiresAuthentication ? Response::HTTP_OK : Response::HTTP_BAD_REQUEST);
+        $requiresRegistration = $this->registrationService->registrationRequired();
+        $response = new Response(null, $requiresRegistration ? Response::HTTP_OK : Response::HTTP_BAD_REQUEST);
 
-        return $this->render('AppBundle:default:authentication.html.twig', [
-            'requiresAuthentication' => $requiresAuthentication,
-            'NameID' => $nameId ?: 'unknown',
+        return $this->render('AppBundle:default:registration.html.twig', [
+            'requiresRegistration' => $requiresRegistration,
+            'NameID' => uniqid('test-prefix-', 'test-entropy'),
         ], $response);
     }
 }
