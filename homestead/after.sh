b048eb64e25414c4831671213ee24502cd9804b9
Martin Roest <roest.martin@gmail.com>
2020-11-09 12:27:25 +0100

    Run on latest node version 10

--- homestead/after.sh
+++ homestead/after.sh
@@ -20,4 +20,4 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-nvm install 8.9.1
+nvm install 10
