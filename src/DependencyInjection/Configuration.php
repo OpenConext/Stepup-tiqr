4b8da689a5b191f4c335e6f0ea2c9e9180c295b3
Michiel Kodde <mkodde@ibuildings.nl>
2021-11-22 13:39:03 +0100

    Address deprecation warnings

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -28,8 +28,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('app');
+        $treeBuilder = new TreeBuilder('app');
+        $rootNode = $treeBuilder->getRootNode();
         $optionsNode = $rootNode->children()
             ->arrayNode('tiqr_library_options')
             ->isRequired();
