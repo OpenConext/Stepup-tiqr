fb18714d30b992aece8612c6b55fe92a9bfb7da0
Michiel Kodde <mkodde@ibuildings.nl>
2024-01-23 09:09:38 +0100

    Replace Annotation routes for Attribute NS
    Also optimized imports with it.

--- dev/Controller/SPController.php
+++ dev/Controller/SPController.php
@@ -37,7 +37,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response as SymfonyResponse;
-use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Attribute\Route;
 
 /**
  * Demo SP.
