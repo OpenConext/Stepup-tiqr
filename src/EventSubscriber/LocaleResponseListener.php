29cfba5a360e78ce8cd03218a4a48806d22cd678
Kay Joosten <joosten2000@gmail.com>
2026-09-15 10:35:38 +0200

    Update Composer dependencies (#433)
    * Document Composer dependency refresh design
    
    * Update Composer dependencies

--- src/EventSubscriber/LocaleResponseListener.php
+++ src/EventSubscriber/LocaleResponseListener.php
@@ -36,7 +36,7 @@ final readonly class LocaleResponseListener implements EventSubscriberInterface
     {
     }
 
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             KernelEvents::REQUEST => ['onKernelRequest'],
